eb2998c1ed92bbd7cbf8f737ca9242521c1bc5c0
Linkaon <noa.andre@efrei.net>
2023-10-23 11:58:49 +0200

    slight change on get song

--- test.py
+++ test.py
@@ -655,6 +655,7 @@ for i in range(len(final_df['Dimension 3'])):
 print(final_df)
 
 
+
 def get_song(songA, songB, songC = None):
     if songC == None:
         if songA == songB:
@@ -662,7 +663,7 @@ def get_song(songA, songB, songC = None):
         else: 
             A = final_df.loc[final_df['Name '] == songA]
             B = final_df.loc[final_df["Name "] == songB]
-            print(A, '\n', B)
+            
             avg_dim_1 = (float(A['Score 1']) + float(B['Score 1']))/2
             avg_dim_2 = (float(A['Score 2']) + float(B['Score 2']))/2
             avg_dim_3 = (float(A['Score 3']) + float(B['Score 3']))/2
@@ -674,22 +675,34 @@ def get_song(songA, songB, songC = None):
             diff_abs2 = float('inf')
             diff_abs3 = float('inf')
             
-            for i in range(len(final_df['Score 1'])):
-                val = float(final_df.loc[i, 'Score 1'])
+            list_score1 = final_df['Score 1'].tolist()
+            list_score2 = final_df['Score 2'].tolist()
+            list_score3 = final_df['Score 3'].tolist()
+            for i in range(len(list_score1)):
+                val = float(list_score1[i])
                 diff = abs(avg_dim_1 - float(val))
                 if diff < diff_abs1:
                     valeur_proche1 = float(val)
                     diff_abs1 = diff
-            
-            for i in range(len(final_df['Score 2'])):
-                val = float(final_df.loc[i, 'Score 2'])
+            rmv1 = None
+            for i in range(len(list_score1)):
+                if list_score1[i] == valeur_proche1:
+                    rmv1 = i
+            del list_score2[rmv1]
+            for i in range(len(list_score2)):
+                val = float(list_score2[i])
                 diff = abs(avg_dim_2 - float(i))
                 if diff < diff_abs2:
                     valeur_proche2 = float(val)
                     diff_abs2 = diff
-            
-            for i in range(len(final_df['Score 3'])):
-                val = float(final_df.loc[i, 'Score 3'])
+            rmv2 = None
+            for i in range(len(list_score2)):
+                if list_score2[i] == valeur_proche2:
+                    rmv2 = i
+            del list_score3[rmv1]
+            del list_score3[rmv2]
+            for i in range(len(list_score3)):
+                val = float(list_score3[i])
                 diff = abs(avg_dim_3 - float(i))
                 if diff < diff_abs3:
                     valeur_proche3 = float(val)
@@ -699,11 +712,15 @@ def get_song(songA, songB, songC = None):
             idx2 = final_df.loc[final_df['Score 2'] == valeur_proche2]
             idx3 = final_df.loc[final_df['Score 3'] == valeur_proche3]
            
-            song1 = str(idx1['Name '])
-            song2 = str(idx2['Name '])
-            song3 = str(idx3['Name '])
+            song1 = idx1['Name ']
+            song2 = idx2['Name ']
+            song3 = idx3['Name ']
             
-            return(song1, song2, song3)
+            song1 = song1.values[0]
+            song2 = song2.values[0]
+            song3 = song3.values[0]
+            list_song = [song1, song2, song3]
+            return(list_song)
     else:
         if songA == songB and songA == songC:
             return songA
@@ -715,9 +732,7 @@ def get_song(songA, songB, songC = None):
             avg_dim_1 = (float(A['Score 1']) + float(B['Score 1']) + float(C['Score 1']))/3
             avg_dim_2 = (float(A['Score 2']) + float(B['Score 2']) + float(C['Score 2']))/3
             avg_dim_3 = (float(A['Score 3']) + float(B['Score 3']) + float(C['Score 3']))/3
-            print('########################################')
-            print(avg_dim_1, avg_dim_2, avg_dim_3)
-            print('########################################')
+            
             valeur_proche1 = None
             valeur_proche2 = None
             valeur_proche3 = None
@@ -725,22 +740,34 @@ def get_song(songA, songB, songC = None):
             diff_abs2 = float('inf')
             diff_abs3 = float('inf')
             
-            for i in range(len(final_df['Score 1'])):
-                val = float(final_df.loc[i, 'Score 1'])
+            list_score1 = final_df['Score 1'].tolist()
+            list_score2 = final_df['Score 2'].tolist()
+            list_score3 = final_df['Score 3'].tolist()
+            for i in range(len(list_score1)):
+                val = float(list_score1[i])
                 diff = abs(avg_dim_1 - float(val))
                 if diff < diff_abs1:
                     valeur_proche1 = float(val)
                     diff_abs1 = diff
-            
-            for i in range(len(final_df['Score 2'])):
-                val = float(final_df.loc[i, 'Score 2'])
+            rmv1 = None
+            for i in range(len(list_score1)):
+                if list_score1[i] == valeur_proche1:
+                    rmv1 = i
+            del list_score2[rmv1]
+            for i in range(len(list_score2)):
+                val = float(list_score2[i])
                 diff = abs(avg_dim_2 - float(i))
                 if diff < diff_abs2:
                     valeur_proche2 = float(val)
                     diff_abs2 = diff
-            
-            for i in range(len(final_df['Score 3'])):
-                val = float(final_df.loc[i, 'Score 3'])
+            rmv2 = None
+            for i in range(len(list_score2)):
+                if list_score2[i] == valeur_proche2:
+                    rmv2 = i
+            del list_score3[rmv1]
+            del list_score3[rmv2]
+            for i in range(len(list_score3)):
+                val = float(list_score3[i])
                 diff = abs(avg_dim_3 - float(i))
                 if diff < diff_abs3:
                     valeur_proche3 = float(val)
@@ -749,12 +776,14 @@ def get_song(songA, songB, songC = None):
             idx1 = final_df.loc[final_df['Score 1'] == valeur_proche1]
             idx2 = final_df.loc[final_df['Score 2'] == valeur_proche2]
             idx3 = final_df.loc[final_df['Score 3'] == valeur_proche3]
-            print(valeur_proche1, "\n", valeur_proche2, "\n", valeur_proche3)
-            print('##############################################')
             
-            song1 = str(idx1['Name '])
-            song2 = str(idx2['Name '])
-            song3 = str(idx3['Name '])
-            print(song1, "\n", song2, "\n", song3)
-            print('##############################################')
-            return (song1, song2, song3)
+            
+            song1 = idx1['Name ']
+            song2 = idx2['Name ']
+            song3 = idx3['Name ']
+            
+            song1 = song1.values[0]
+            song2 = song2.values[0]
+            song3 = song3.values[0]
+            list_song = [song1, song2, song3]
+            return(list_song)
